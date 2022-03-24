5c3708938150f92264c92242f1056ecead4cd547
JintekiWarrior <jintekiwarrior@gmail.com>
2022-03-24 09:40:58 -0400

    chenged tab spacing from 4 to 2

--- index.js
+++ index.js
@@ -16,15 +16,68 @@ const promptUser = () => {
 					return false
 				}
 			}
-		}
+		},
 		// Description
-		// Table of Contents
-		// Installation 
+		{
+			type: 'input',
+			name: 'description',
+			message: 'What is the description?',
+			validate: descriptionInput => {
+				if (descriptionInput) {
+					return true
+				} else {
+					console.log('Please enter a description for the project.')
+					return false
+				}
+			}
+		},
+		// Installation
+		{
+			type: 'input',
+			name: 'installation',
+			message: 'What are the steps required to install your project?'
+		}, 
 		// Usage
-		// License 
+		{
+			type: 'input',
+			name: 'usage',
+			message: 'What are the instructions and examples for use?',
+			validate: usageInput => {
+				if (usageInput) {
+					return true
+				} else {
+					console.log('Please enter the instructions and examples of use.')
+					return false
+				}
+			}
+		},
 		// Contributing
+		{
+			type: 'input',
+			name: 'contributions',
+			message: 'What contributions do you need to list?'
+		},
 		// Tests
+		{
+			type: 'input',
+			name: 'tests',
+			message: 'What tests are included and how are they used?'
+		},
 		// FAQ
+		{
+			type: 'input',
+			name: 'questions',
+			message: 'What is your email so that users can ask you questions?',
+			validate: questionsInput => {
+				if (questionsInput) {
+					return true
+				} else {
+					console.log('Please enter your email.')
+					return false
+				}
+			}
+		}
+		// License 
   ])
 	.then(promptData => console.log(promptData))
 }
